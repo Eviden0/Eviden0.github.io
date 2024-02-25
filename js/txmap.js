bbeac7a3b809a287c4dcc921f9f9d1e97cff68cb
Eviden0 <1538279017@qq.com>
2024-02-26 00:46:03 +0800

    Site updated: 2024-02-26 00:46:02

--- js/txmap.js
+++ js/txmap.js
@@ -31,7 +31,7 @@ function getDistance(e1, n1, e2, n2) {
 
 function showWelcome() {
 
-    let dist = getDistance(113.34499552, 23.15537143, ipLoacation.result.location.lng, ipLoacation.result.location.lat); //这里换成自己的经纬度
+    let dist = getDistance(112.381002, 26.966537, ipLoacation.result.location.lng, ipLoacation.result.location.lat); //这里换成自己的经纬度
     let pos = ipLoacation.result.ad_info.nation;
     let ip;
     let posdesc;
